415bde16b2f4ff18d34be2f88788710f8e8b033f
unknown <Salam P>
2023-04-01 09:18:18 +0700

    Back

--- function.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.navButton').forEach(item => {
-  item.addEventListener('click', () => {
-    if (!(item.classList.contains('active'))) {
-      document.querySelector('.active').classList.remove('active');
-      item.classList.add('active');
-    }
-  })
-})
